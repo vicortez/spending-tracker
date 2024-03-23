cd32b4110ea1c9383280fb910488bed5634c9400
Victor Cortez <vicortezdev@gmail.com>
2024-03-22 23:52:25 -0300

    add secret graph page

--- lib/pages/test_page.dart
+++ lib/pages/test_page.dart
@@ -1,85 +1,7 @@
 import 'dart:collection';
+import 'dart:math';
 
-//
-// class TestPage extends StatelessWidget {
-//   const TestPage({
-//     super.key,
-//   });
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     var categoryState = context.watch<CategoryState>();
-//     var domainState = context.watch<DomainState>();
-//     var expenseState = context.watch<ExpenseState>();
-//
-//     var categories = categoryState.getEnabledCategories();
-//     List<Domain> domains = domainState.domains;
-//     List<Expense> expenses = expenseState.expenses;
-//
-//     // List<AccCategory> accCats = [];
-//     HashMap<int, double> accCats = HashMap();
-//     for (var expense in expenses) {
-//       int catId = expense.categoryId ?? 999;
-//       accCats.putIfAbsent(catId, () => 0);
-//       accCats[catId] = accCats[catId]! + expense.amount;
-//     }
-//     List<AccCategory> accCatList = accCats.entries.map((e) => AccCategory(e.key, e.value)).toList();
-//     accCatList.sort((a, b) => a.acc.compareTo(b.acc));
-//
-//     // domains.sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
-//     // categories.sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
-//
-//     return Scaffold(
-//       appBar: AppBar(
-//         leading: const BackButton(),
-//         title: const Text("Test ground"),
-//       ),
-//       body: Column(
-//         mainAxisSize: MainAxisSize.min,
-//         children: [
-//           Text("Top 5 categories with most expenses"),
-//           Expanded(
-//             child: PieChart(
-//               PieChartData(
-//                 sections: [
-//                   PieChartSectionData(
-//                       value: accCatList[0].acc,
-//                       color: Colors.red,
-//                       title: categories.firstWhere((element) => accCatList[0].catId == element.id).name,
-//                       titleStyle: TextStyle(fontSize: 24)),
-//                   PieChartSectionData(
-//                       value: accCatList[1].acc,
-//                       color: Colors.yellow,
-//                       title: categories.firstWhere((element) => accCatList[1].catId == element.id).name,
-//                       titleStyle: TextStyle(fontSize: 24)),
-//                   PieChartSectionData(
-//                       value: accCatList[2].acc,
-//                       color: Colors.green,
-//                       title: categories.firstWhere((element) => accCatList[2].catId == element.id).name,
-//                       titleStyle: TextStyle(fontSize: 24)),
-//                   PieChartSectionData(
-//                       value: accCatList[3].acc,
-//                       color: Colors.blue,
-//                       title: categories.firstWhere((element) => accCatList[3].catId == element.id).name,
-//                       titleStyle: TextStyle(fontSize: 24)),
-//                   PieChartSectionData(
-//                       value: accCatList[4].acc,
-//                       color: Colors.purple,
-//                       title: categories.firstWhere((element) => accCatList[4].catId == element.id).name,
-//                       titleStyle: TextStyle(fontSize: 24)),
-//                 ],
-//                 sectionsSpace: 0,
-//                 centerSpaceRadius: 40,
-//                 borderData: FlBorderData(show: false),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
+import 'package:collection/collection.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -148,20 +70,30 @@ class TestPage extends StatelessWidget {
       accCats[catId] = accCats[catId]! + expense.amount;
     }
     List<AccCategory> accCatList = accCats.entries.map((e) => AccCategory(e.key, e.value)).toList();
-    accCatList.sort((a, b) => a.acc.compareTo(b.acc));
+    accCatList.sort((a, b) => -a.acc.compareTo(b.acc));
 
-    var colors = [Colors.yellow, Colors.blue, Colors.purple, Colors.green, Colors.red];
+    var colors = [
+      Colors.yellow,
+      Colors.blue,
+      Colors.purple,
+      Colors.green,
+      Colors.red,
+      Colors.grey,
+      Colors.white,
+      Colors.brown,
+      Colors.orange
+    ];
 
     final List<PieChartSectionData> list = [];
-    for (int i = 0; i < accCatList.length; i++) {
+    for (int i = 0; i < min(accCatList.length, 20); i++) {
       var accCat = accCatList[i];
       const double radius = 40.0;
       final data = PieChartSectionData(
-        color: colors[i],
+        color: colors[i % colors.length],
         value: accCat.acc,
         radius: radius,
-        title: (categories.firstWhere((element) => accCatList[i].catId == element.id)?.name ?? "<noCat>") +
-            " \$${accCat.acc}",
+        title:
+            "${categories.firstWhereOrNull((element) => accCatList[i].catId == element.id)?.name ?? "<noCat>"} \$${accCat.acc}",
         titleStyle: TextStyle(fontSize: 18),
       );
       list.add(data);
